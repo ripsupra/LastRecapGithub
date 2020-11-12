39666b3425b1d0d49580c365ae5187c4b1ccad48
Adil Imanaliev <imanaliev.adil@gmail.com>
2020-11-12 10:35:05 -0500

    just an extra print statement to follow the commit step

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Last day of Github class to Recap everything we have done");
+        System.out.println();
     }
 }
