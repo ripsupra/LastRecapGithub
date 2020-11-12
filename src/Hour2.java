304c49b0a455d287940bd63f90a2bb0e438b67ec
Adil Imanaliev <imanaliev.adil@gmail.com>
2020-11-12 11:20:22 -0500

    last commit message for the Hour2 class

--- src/Hour2.java
+++ src/Hour2.java
@@ -2,5 +2,6 @@ public class Hour2 {
     public static void main(String[] args) {
         System.out.println("This is hour 2");
         System.out.println("New message");
+        System.out.println("Last commit message");
     }
 }
